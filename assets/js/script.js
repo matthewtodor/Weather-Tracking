c9cca510a5a867f1168ccc3dc3ff839d2d217b4f
Matthew Todor <unclejunglegeorge@gmail.com>
2022-08-21 00:38:43 -0600

    added variables for today's weather, will build JQuery for variable output to DOM next

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,17 @@ $("#city-search").on("keydown", function () {
 	$("#city-search").removeClass("is-invalid");
 	$("#city-empty-alert").empty();
 });
-console.log(window);
-if ($("window").screen.width < 768) {
-	$("#days").attr("type", "number");
+console.log(window.screen.width);
+if (window.screen.width < 768) {
+	$("#days").attr("type", "select");
+	$("#days").attr("value", "");
 } else {
 	$("#days").attr("type", "range");
 }
 $("#searchBtn").on("click", function (e) {
 	e.preventDefault();
 	var city = "?city=" + $("#city-search").val().replace(/\s/g, "").toLowerCase();
-	var units = "?units=" + $("input[name=units]:radio:checked").val();
+	var units = "&units=" + $("input[name=units]:radio:checked").val();
 	var days = "&days=" + $("#days").val();
 
 	if ($("#city-search").val().length == 0) {
@@ -31,10 +32,22 @@ function getWeather(city, units, days) {
 		method: "get",
 		url: "https://api.weatherbit.io/v2.0/forecast/daily" + city + units + days + apiKey,
 	}).then(function (response) {
-		console.log(response);
+		console.log("https://api.weatherbit.io/v2.0/forecast/daily" + city + units + days + apiKey);
+		console.log(response.data[0]);
+		$("#todays-forcast").empty();
+		var highTemp = response.data[0].high_temp;
+		var lowTemp = response.data[0].low_temp;
+		var currentTemp = response.data[0].temp;
+		var windSpeed = response.data[0].wind_spd;
+		var windDirection = response.data[0].wind_cdir_full;
+		var weatherDescr = response.data[0].weather.description;
+		var weatherIcon = response.data[0].weather.icon;
+		console.log(highTemp, lowTemp, currentTemp, windSpeed, windDirection, weatherDescr, weatherIcon);
 		$("#city-search").val("");
 	});
 }
 function showVal(newVal) {
 	$("#number-of-days").text(newVal);
 }
+
+// function todaysWeather();
